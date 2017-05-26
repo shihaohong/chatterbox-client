14ac4ff37e32f4d77235fa50e2b11290c779789d
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-26 15:55:58 -0700

    Completed send and fetch AJAX functions, as well as defining the app.server url

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,36 @@
 // YOUR CODE HERE:
+var app = {
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
+  init: function() {},
+  send: function(data) {
+    $.ajax({
+      type: 'POST',
+      url: this.server,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function(data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function(data) {
+    $.ajax({
+      type: 'GET',
+      url: this.server,
+      data: JSON.stringify(data),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Load was performed');
+      }
+    });
+  },
+  clearMessages: function() {},
+  renderMessage: function(message) {
+    
+    $("#chats").append(message);
+  },
+  renderRoom: function() {}
+};
